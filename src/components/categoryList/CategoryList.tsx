ee9b998970b420dd2f6eccc92f6c8b29dabeba20
nadeeraka <global-nadeerakan@gmail.com>
2023-12-16 02:49:15 +0530

    UI UX improvements on card and other components

--- src/components/categoryList/CategoryList.tsx
+++ src/components/categoryList/CategoryList.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import styles from "./categoryList.module.css";
+import Link from "next/link";
+import Image from "next/image";
+import Pic1 from "../../../public/evie-s-ti6vZMUC6S4-unsplash.jpg";
 
-const CategoryList = () => {
-  return <div>CategoryList</div>;
+const CategoryList = async () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Popular Categories</h1>
+      <div className={styles.categories}>
+        <Image
+          src={Pic1}
+          alt="pic"
+          width={32}
+          height={32}
+          className={styles.image}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default CategoryList;
--- src/components/menuPosts/MenuPosts.tsx
+++ src/components/menuPosts/MenuPosts.tsx
@@ -1,7 +1,24 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import styles from "./menuPosts.module.css";
 
 const MenuPosts = () => {
-  return <div>MenuPosts</div>;
+  return (
+    <div className={styles.items}>
+      <Link href="/" className={styles.item}>
+        <div className={styles.textContainer}>
+          <h3 className={styles.postTitle}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </h3>
+          <div className={styles.detail}>
+            <span className={styles.username}>John Doe</span>
+            <span className={styles.date}> - 10.03.2023</span>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
 };
 
 export default MenuPosts;
